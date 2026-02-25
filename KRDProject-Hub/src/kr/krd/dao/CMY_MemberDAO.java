5a05479d05f70d27455f59b4102d46232ec6f9d7
sist <sist@DESKTOP-9QG8QLN>
2026-02-25 16:05:52 +0900

    대략적인 메인 화면, 로그인 화면, 로그인 기능 구현

---
+++ KRDProject-Hub/src/kr/krd/dao/CMY_MemberDAO.java
@@ -0,0 +1,71 @@
+package kr.krd.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import kr.util.DBUtil;
+
+public class CMY_MemberDAO {
+	
+	
+	//로그인 메서드
+	public String userLogin(String user_id, String user_pw) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		ResultSet rs = null;
+		String real_id = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT * FROM USERINFO WHERE USER_ID = ? AND USER_PWD = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user_id);
+			pstmt.setString(2, user_pw);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				String user_name = rs.getString("USER_NAME");
+				System.out.println(user_name + "님 환영합니다.");
+				real_id = rs.getString("user_id");	//일치하면 real_id에 유저 아이디를 넣는다
+				
+			}else {
+				System.out.println("아이디 혹은 비밀번호가 일치 하지 않습니다.");
+				real_id = "0";	//틀리면 "0"을 넣는다.
+				
+			}
+		}
+		catch(Exception e){e.printStackTrace();}
+		finally {DBUtil.excuteClose(rs, pstmt, conn);}
+		return real_id;
+	}
+	
+	//사용자의 권한을 반환하는 메서드
+	public String getUserRole(String user_id) {
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		ResultSet rs = null;
+		String real_role = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			sql = "SELECT user_role_cd FROM USERINFO WHERE USER_ID = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user_id);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				real_role = rs.getString("user_role_cd");	//일치하면 real_id에 유저 아이디를 넣는다
+				System.out.println("권한 디버그용 : "+real_role);
+			}
+		}
+		catch(Exception e){e.printStackTrace();}
+		finally {DBUtil.excuteClose(rs, pstmt, conn);}
+		return real_role;
+	}
+	
+	
+	//로그아웃 메서드
+	
+}
--- KRDProject-Hub/src/kr/krd/main/CMY_KRDUserMain.java
+++ KRDProject-Hub/src/kr/krd/main/CMY_KRDUserMain.java
@@ -3,13 +3,13 @@ package kr.krd.main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import kr.krd.dao.MemberDAO;
+import kr.krd.dao.CMY_MemberDAO;
 
 public class CMY_KRDUserMain {
 	private BufferedReader br;
-	private MemberDAO dao;
+	private CMY_MemberDAO dao;
 	private String cust_id;//로그인한 회원 아이디
+	private String role; //사용자의 역할
 	private boolean login;//로그인 여부(로그인:true,로그아웃:false)
 	
 	public CMY_KRDUserMain() {
@@ -17,7 +17,7 @@ public class CMY_KRDUserMain {
 			br = new BufferedReader(
 					new InputStreamReader(
 							     System.in));
-			dao = new MemberDAO();
+			dao = new CMY_MemberDAO();
 			callMenu();
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -31,17 +31,62 @@ public class CMY_KRDUserMain {
 	//메뉴
 	private void callMenu()throws IOException{
 		while(true) {
-			System.out.print("1.로그인,2.회원가입,3.종료>");
+			System.out.println("┌────────────────────────────────────────────────────────┐");
+			System.out.println("│							 │");
+			System.out.println("│	국가 연구과제 관리 프로그램	「KRD Hubs」		 │");
+			System.out.println("│							 │");
+			System.out.println("│	1. 로그인						 │");
+			System.out.println("│	2. 회원가입					 │");
+			System.out.println("│	3. 공고조회					 │");
+			System.out.println("│	4. 종료						 │");
+			System.out.println("│							 │");
+			System.out.println("│						ver.1.0	 │");
+			System.out.println("└────────────────────────────────────────────────────────┘");
+			System.out.println("［원하시는 메뉴를 선택하세요 ]");
+			System.out.print(">>");
 			try {
-				int no = Integer.parseInt(br.readLine());
-				if(no == 1) {//로그인
+				int MenuNo = Integer.parseInt(br.readLine());
+				if(MenuNo == 1) {//로그인
+					System.out.println("┌────────────────────────────────────────┐");
+					System.out.println("│					 │ ");
+					System.out.println("│	로그인[1]	 			 │");
+					System.out.println("│					 │");
+					System.out.println("│	ID	________________	 │");
+					System.out.println("│	PW	________________	 │");
+					System.out.println("│					 │");
+					System.out.println("│	아이디 / 비밀번호 찾기 [2]		 │");
+					System.out.println("│					 │");
+					System.out.println("└────────────────────────────────────────┘");
+					System.out.println(">> 진행하실 사항을 선택하세요 [1] : 로그인 | [2] : 아이디/비밀번호 찾기 :  | [3] 뒤로 가기");
+					int secMenuNo = Integer.parseInt(br.readLine());
+					if(secMenuNo == 1) {
+						//로그인 진행
+						System.out.print("ID : ");
+						String user_id = br.readLine();
+						System.out.print("PW : ");
+						String user_pw = br.readLine();
+						//메서드이름(user_id,user_pw);
+						cust_id=dao.userLogin(user_id, user_pw);
+						if(!cust_id.equals("0") && !cust_id.equals(null)) {// cust_id가 0 또는 null이 아니면 : 즉 유저 아이디 값이 존재함.
+							
+							login = true;					//사용자의 로그인 상태를 TRUE로 변경함
+							role = dao.getUserRole(cust_id);//사용자의 권한을 반환하는 메서드를 사용하여 role에 해당하는 권한을 넣음
+							break;
+						}
+					}else if(secMenuNo == 2) {
+						//아이디/비밀번호 찾기로 진행
+					}else if(secMenuNo == 3) {
+						continue;
+					}
+				}else if(MenuNo == 2) {//회원가입
 					
-				}else if(no == 2) {//회원가입
+				}else if(MenuNo == 3) {//공고조회
 					
-				}else if(no == 3) {//종료
+				}else if(MenuNo == 4) {
 					System.out.println("프로그램 종료");
 					break;
-				}else {
+				}
+				else {
 					System.out.println("잘못 입력했습니다.");
 				}
 			}catch(NumberFormatException e) {
@@ -50,29 +95,46 @@ public class CMY_KRDUserMain {
 		}//end of while
 		//로그인시 보여지는 메뉴
 		while(login) {
-			System.out.print(
-			"1.회원 정보,2.상품 구매,3.구매 내역,4.종료>");
-			try {
-				int no = Integer.parseInt(
-						           br.readLine());
-				if(no == 1) {//회원 정보
-					
-				}else if(no == 2) {//상품 구매
-					
-				}else if(no == 3) {//구매 내역
-					
-				}else if(no == 4) {//종료
-					//종료
-					System.out.println("프로그램 종료");
-					break;
-				}else {
-					System.out.println(
-							"잘못 입력했습니다.");
-				}
-			}catch(NumberFormatException e) {
-				System.out.println("[숫자만 입력 가능]");
+			System.out.println("일단 뭐든 로그인으로 들어왔어");
+			if(role.equals("ADM")) {
+				System.out.println("-".repeat(20));
+				System.out.println("어드민으로 진입시 구현될 화면");
+				//로그아웃은 개인 구현
+				System.out.println("-".repeat(20));
+				break;
+			}else if(role.equals("AGY")) {
+				System.out.println("-".repeat(20));
+				System.out.println("기관 관리자로 진입시 구현될 화면");
+				//로그아웃은 개인 구현
+				System.out.println("-".repeat(20));
+				break;
+			}else if(role.equals("RESI")) {
+				System.out.println("-".repeat(20));
+				System.out.println("개인 연구자로 진입시 구현될 화면");
+				//로그아웃은 개인 구현
+				System.out.println("-".repeat(20));
+				break;
+			}else if(role.equals("RESO")) {
+				System.out.println("-".repeat(20));
+				System.out.println("단체 연구자로 진입시 구현될 화면");
+				//로그아웃은 개인 구현
+				System.out.println("-".repeat(20));
+				break;
+			}else if(role.equals("REV")) {
+				System.out.println("-".repeat(20));
+				System.out.println("평가위원으로 진입시 구현될 화면");
+				//로그아웃은 개인 구현
+				System.out.println("-".repeat(20));
+				break;
+			}else if(role.equals("GST")){
+				System.out.println("GST는 아직 미구현이야...");
+				//로그아웃은 개인 구현
+				break;
+			}else {
+				System.out.println("로그인 했는데 권한이 없어???");
 			}
 		}//end of while
+		System.out.println("일단 프로그램 종료 부분");
 	}
 	
 	public static void main(String[] args) {
