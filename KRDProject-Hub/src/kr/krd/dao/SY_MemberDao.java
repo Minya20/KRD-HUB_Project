e59210bdc5b2f9b16b359a814f059aa42c2a8acc
user <user@DESKTOP-P4V4Q35>
2026-02-25 17:09:23 +0900

    회원가입

--- KRDProject-Hub/src/kr/krd/dao/SY_MemberDao.java
+++ KRDProject-Hub/src/kr/krd/dao/SY_MemberDao.java
@@ -9,7 +9,7 @@ import kr.util.DBUtil;
 import java.sql.Date;
 
 public class SY_MemberDao {
-	  
+	    
 	private BufferedReader br;
 //회원가입(회원유형 선택을 곁들인)
 	public void insertMember(String user_id, String user_pwd, String user_name, String user_email, String user_birth_dt, String user_phone_no,
